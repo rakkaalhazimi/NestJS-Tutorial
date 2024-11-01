2f4de25cef210418448f7a66a89e97da23069515
Rakka Alhazimi <rakkakeren@gmail.com>
2024-11-02 06:56:59 +0700

    rm: conflict prompt

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,10 @@ import { AppService } from './app.service';
 import { CrudModule } from './tutorial-crud/crud.module';
 import { DecoratorModule } from './tutorial-decorator/decorator.module';
 import { DependenciesModule } from './tutorial-dependencies/tutorial-dependencies.module';
-<<<<<<< HEAD
 import { TestModule } from './tutorial-test/test.module';
 
 @Module({
   imports: [CrudModule, DecoratorModule, DependenciesModule, TestModule],
-=======
-
-@Module({
-  imports: [CrudModule, DecoratorModule, DependenciesModule],
->>>>>>> f0b906e100a903270574720963056e66c9634cd1
   controllers: [AppController],
   providers: [AppService],
 })
